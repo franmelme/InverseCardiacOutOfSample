ca5ca23d8a46de420554164305a07a792c474cea
franmelme <franmelme@users.noreply.github.com>
2022-10-10 08:58:53 +0200

    Update OutOfSampleInversion.m

--- OutOfSampleInversion.m
+++ OutOfSampleInversion.m
@@ -2,20 +2,6 @@ function [output] = OutOfSampleInversion(ve,H)
 % OutOfSampleInversion Computes the ECG inverse solution appliying out of
 % sample technique according with Generalization and Regularization for Inverse Cardiac Estimators
 
-%  Copyright (C) 2021  F. M. Melgarejo-Meseguer, E. Everss-Villalba, M. Gutierrez-Fernandez-Calvillo, 
-%                      S. Munoz-Romero, F.-J. Gimeno-Blanes, A. Garcia-Alberola, and J.-L. Rojo-Alvarez
-%
-%    This program is free software: you can redistribute it and/or modify
-%    it under the terms of the GNU General Public License as published by
-%    the Free Software Foundation version 3.
-%
-%    This program is distributed in the hope that it will be useful,
-%    but WITHOUT ANY WARRANTY; without even the implied warranty of
-%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%    GNU General Public License for more details.
-
-%    You should have received a copy of the GNU General Public License
-%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 % %%%%%%%%% INPUT %%%%%%%%%%%%%
 % ve -> (NxS matrix) Voltage measured on torso.
